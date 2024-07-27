35749403135d3b7ad832c271273b22b4ce446b95
amish12822 <amishofficial23@gmail.com>
2024-07-27 14:14:49 +0530

    complete program table

---
+++ table.java
@@ -0,0 +1,14 @@
+/**
+ * table
+ */
+public class table {
+    public static void main(String[] args) {
+        int n = 19; // user enter value 
+        
+        for( int i = 1; i<=10; i++){
+            
+            System.out.println( n + " x " + i + " = " + n*i );
+        }
+    }
+    
+}
